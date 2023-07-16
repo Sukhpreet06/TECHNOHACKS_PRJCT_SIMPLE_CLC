b93b337a1eb3b8aac3eed494fdad62514745a6d0
Sukhpreet06 <137524883+Sukhpreet06@users.noreply.github.com>
2023-07-16 15:34:58 +0530

    Add files via upload

---
+++ thsimplclc.js
@@ -0,0 +1,18 @@
+function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
+  function deleteLastChar() {
+    let result = document.getElementById("result").value;
+    document.getElementById("result").value = result.slice(0, -1);
+  }
+  
+  function appendCharacter(character) {
+    document.getElementById("result").value += character;
+  }
+  
+  function calculate() {
+    let result = document.getElementById("result").value;
+    let answer = eval(result);
+    document.getElementById("result").value = answer;
+  }
